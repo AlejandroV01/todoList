302f8e59a91fb5892d85939a7b696f3848a82b1f
AlejandroV01 <alexvera0109@gmail.com>
2022-08-18 21:21:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+const App = () => {
+  const [todos, setTodos] = useState([]);
 
-function App() {
+  const addTask = (text) => {
+    if (!text || /^\s*$/.test(text)) {
+      alert("enter valid input");
+      return;
+    }
+    const newTodo = { text: text, id: Math.random() * 1000, checked: false };
+    setTodos([newTodo, ...todos]);
+  };
+
+  const handleDelete = (id) => {
+    const newArr = todos.filter((todo) => todo.id !== id);
+    setTodos(newArr);
+  };
+
+  const handleCheck = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !todo.checked;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <div className="container">
+        <TodoForm addTask={addTask} />
+        <TodoList
+          todos={todos}
+          handleDelete={handleDelete}
+          handleCheck={handleCheck}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
